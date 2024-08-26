2efa0fbfcd1aaa6b335a8c038f23f6a9193d31f6
Fiza <fiza.arif@sakhglobal.com>
2024-08-26 12:37:56 +0530

    12:37 am changes pushed

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const io = socketIO(server, {
     origin: "https://sneakspeak.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: "*",
-    credentials: true
+    credentials: true //Trying credentials true
   }
 });
 
